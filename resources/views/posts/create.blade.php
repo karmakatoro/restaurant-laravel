43c744f9b089f5f5f6ae7447470ee642c762662f
karmakatoro <karmakatoro@gmail.com>
2023-04-18 09:42:09 +0200

    error messages displayed

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,12 +4,6 @@
             {{ __('Creer un post') }}
         </h2>
     </x-slot>
-{{--
-    <form action="{{ route("posts.store") }}" method="post">
-        @csrf
-        <input type="text" name="title">
-        <button type="submit">Envoyer</button>
-    </form> --}}
 
     <div class="flex flex-col justify-between">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-10 ">
@@ -17,9 +11,7 @@
                 Ajouter un post
             </h2>
             <div class="my-5">
-                @foreach ($errors->all() as $error)
-                <span class="block text-red-500">{{ $error }}</span>
-                @endforeach
+
             </div>
             <form class="w-full max-w-lg" method="post" action="{{ route('posts.store') }}" enctype="multipart/form-data">
                 @csrf
@@ -29,6 +21,9 @@
                             Titre du post
                         </label>
                         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" name="title" id="title" type="text" placeholder="Titre du post">
+                        @if($errors->has('title'))
+                        <p class="text-red-500 text-xs italic">{{ $errors->first('title') }}</p>
+                        @endif
                     </div>
                     <div class="w-full md:w-1/2 px-3">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="category">
@@ -40,7 +35,9 @@
                                 <option value="{{ $category->id }}">{{ $category->name }}</option>
                                 @endforeach
                             </select>
-
+                            @if($errors->has('category'))
+                            <p class="text-red-500 text-xs italic">{{ $errors->first('category') }}</p>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -51,11 +48,17 @@
                         </label>
                         <textarea class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="content" id="content">
                 </textarea>
+                        @if($errors->has('content'))
+                        <p class="text-red-500 text-xs italic">{{ $errors->first('content') }}</p>
+                        @endif
                     </div>
                 </div>
                 <div class="flex flex-wrap -mx-3 mb-6">
                     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" name="image" id="image" type="file">
+                        @if($errors->has('image'))
+                        <p class="text-red-500 text-xs italic">{{ $errors->first('image') }}</p>
+                        @endif
                     </div>
                     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                         <button type="submit" class="appearance-none block w-full bg-indigo-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white">Ajouter</button>
